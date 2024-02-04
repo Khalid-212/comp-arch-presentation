44a85317687bef0bd59045ef7876f4217b7ccb91
khalid <khalid.ibr212@gmail.com>
2024-02-04 21:35:51 +0300

    touch navigation added

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
 
   function showSlide(index) {
     slides.forEach((slide, i) => {
-      if (i === index) {
-        slide.classList.add("active-slide");
-      } else {
-        slide.classList.remove("active-slide");
-      }
-      if (i === index - 1 || (index === 0 && i === slides.length - 1)) {
-        slide.classList.add("prev-slide");
-      }
+      slide.classList.toggle("active-slide", i === index);
     });
   }
 
   document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-    } else if (e.key === "ArrowLeft") {
-      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-      showSlide(currentSlide);
+    if (e.key === "ArrowRight" && currentSlide < slides.length - 1) {
+      currentSlide++;
+    } else if (e.key === "ArrowLeft" && currentSlide > 0) {
+      currentSlide--;
     }
+    showSlide(currentSlide);
+  });
+
+  document.addEventListener("click", function () {
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+    } else {
+      currentSlide = 0;
+    }
+    showSlide(currentSlide);
   });
 
   showSlide(currentSlide);
